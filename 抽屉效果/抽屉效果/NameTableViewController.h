b4a2e987b0f7fa29edb45445cd1858e5059fffc1
于滢 <yuying@yuyingdemacbook-pro.local>
2016-03-13 19:54:50 +0800

    创建了xib文件噢

---
+++ 抽屉效果/抽屉效果/NameTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NameTableViewController.h
+//  抽屉效果
+//
+//  Created by 于滢 on 16/3/13.
+//  Copyright © 2016年 Wolibear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NameTableViewController : UITableViewController
+
+@end
